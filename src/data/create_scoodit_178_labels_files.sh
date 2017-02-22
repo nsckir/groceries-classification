50f7acc6d72c5fa4dbd439037b1aeea472d93dc0
Kiril <kiril.schewzow@googlemail.com>
2017-02-22 18:10:37 +0100

    labels from 0

--- src/data/create_scoodit_178_labels_files.sh
+++ src/data/create_scoodit_178_labels_files.sh
@@ -6,5 +6,5 @@ LABELS_FILE="${HOME}/PycharmProjects/scoodit_image_classification/data/processed
 SLIM_LABELS_FILE="${HOME}/PycharmProjects/scoodit_image_classification/data/processed/scoodit_178/slim_labels.txt"
 #touch "${LABELS_FILE}"
 TRAIN_DIRECTORY="${HOME}/PycharmProjects/scoodit_image_classification/data/raw/scoodit_178/train/"
-ls -1 ${TRAIN_DIRECTORY} |  sed 's/\///' | sort | awk '{print NR":"$0}' > ${SLIM_LABELS_FILE}
+ls -1 ${TRAIN_DIRECTORY} |  sed 's/\///' | sort | awk '{print NR-1":"$0}' > ${SLIM_LABELS_FILE}
 ls -1 ${TRAIN_DIRECTORY} |  sed 's/\///' | sort  > ${LABELS_FILE}
