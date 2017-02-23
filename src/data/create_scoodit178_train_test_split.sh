a5d5bb10eacc2a12ab7bdd0ea4c4eee5d9c4573c
Kiril <kiril.schewzow@googlemail.com>
2017-02-23 16:09:19 +0100

    included tensorflow slim

--- src/data/create_scoodit178_train_test_split.sh
+++ src/data/create_scoodit178_train_test_split.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# Data: aws s3 sync s3://scoodit.image.classification.data/raw_data/  ${DATA_DIR}/ --exclude "*" --include "*.tar"
+
+# This script assumes the following structure  in ../../data/raw/scoodit_178/
+#  /label1/image0.jpeg
+#  /label1/image1.jpg
+#  /label1/image2.png
+#  ...
+#  /label2/weird-image.jpeg
+#  /label2/my-image.jpeg
+#  /label2/my-image.JPG
+
 cd ../../data/raw/scoodit_178/
 mkdir -p train;
 mkdir -p test; 
 for i in ./*; do 
 	c=`basename "$i"`; 
-	frac=0.95
+	fraction=0.95
 	mkdir -p train/"$c";
 	mkdir -p test/"$c"; 
 	count=$(find "$i"/*.JPEG -maxdepth 1 -type f|wc -l); 
-	tr_samples=$(expr $count*$frac | bc)
+	tr_samples=$(expr ${count}*${fraction} | bc)
 	round_tr_samples=${tr_samples%.*}
 	echo "$c"
-	echo $count; 
-	echo $round_tr_samples; 
-    for j in `ls "$i"/*.JPEG | shuf | head -n $round_tr_samples`; do
+	echo ${count};
+	echo ${round_tr_samples};
+    for j in `ls "$i"/*.JPEG | shuf | head -n ${round_tr_samples}`; do
         mv "$j" train/"$c"/
     done
     mv "$i"/*.JPEG test/"$c"/
