0934a6d3146710e76ec04ede47ffba6a0f3d767f
Kiril <kiril.schewzow@googlemail.com>
2017-02-25 08:16:07 +0100

    more params

--- src/slim/scripts/finetune_inception_v3_on_scoodit_178.sh
+++ src/slim/scripts/finetune_inception_v3_on_scoodit_178.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 #
 # This script performs the following operations:
-# 1. Downloads the Flowers dataset
-# 2. Fine-tunes an InceptionV3 model on the Flowers training set.
-# 3. Evaluates the model on the Flowers validation set.
+# 2. Fine-tunes an InceptionV3 model on the scoodit_178 training set.
+# 3. Evaluates the model on the scoodit_178 validation set.
 #
 # Usage:
 # cd slim
-# ./slim/scripts/finetune_inceptionv3_on_flowers.sh 32 1 4 4
+# ./slim/scripts/finetune_inceptionv3_on_scoodit_178.sh 32 1 4 4 1000 500
 
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=${HOME}/PycharmProjects/scoodit_image_classification/models/downloaded_tf_models
-
-# Where the training (fine-tuned) checkpoint and logs will be saved to.
-# TRAIN_DIR=${HOME}/PycharmProjects/scoodit_image_classification/models/inception_v3
 
 # Where the dataset is saved to.
 DATASET_DIR=${HOME}/PycharmProjects/scoodit_image_classification/data/processed/scoodit_178/
@@ -22,12 +18,14 @@ BATCH=$1
 CLONES=$2
 READERS=$3
 THREADS=$4
+STEPS1=$5
+STEPS2=$6
 DATASET=scoodit_178
 MODEL=inception_v3
 SUFFIX=bt_${BATCH}_cl_${CLONES}_r_${READERS}_thr_${THREADS}
+
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
 TRAIN_DIR=${HOME}/PycharmProjects/scoodit_image_classification/models/${MODEL}/${DATASET}/${SUFFIX}
-
 
 # Download the pre-trained checkpoint.
 if [ ! -d "$PRETRAINED_CHECKPOINT_DIR" ]; then
@@ -40,7 +38,7 @@ if [ ! -f ${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt ]; then
   rm inception_v3_2016_08_28.tar.gz
 fi
 
-# Fine-tune only the new layers for 1000 steps.
+# Fine-tune only the new layers.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR} \
   --dataset_name=${DATASET} \
@@ -50,7 +48,7 @@ python train_image_classifier.py \
   --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
   --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
-  --max_number_of_steps=1000 \
+  --max_number_of_steps=${STEPS1} \
   --learning_rate=0.01 \
   --learning_rate_decay_type=fixed \
   --save_interval_secs=60 \
@@ -72,7 +70,7 @@ python eval_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL}
 
-# Fine-tune all the new layers for 500 steps.
+# Fine-tune all the new layers.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR}/all \
   --dataset_name=${DATASET} \
@@ -80,7 +78,7 @@ python train_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL} \
   --checkpoint_path=${TRAIN_DIR} \
-  --max_number_of_steps=500 \
+  --max_number_of_steps=${STEPS2} \
   --learning_rate=0.0001 \
   --learning_rate_decay_type=fixed \
   --save_interval_secs=60 \
