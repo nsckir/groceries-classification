f20f10e9826f87d9b3512235014435f7354b159d
Kiril <kiril.schewzow@googlemail.com>
2017-02-25 14:28:25 +0100

    params test in scoodit_178

--- src/slim/scripts/finetune_all_inception_v3_on_scoodit_178_param_test.sh
+++ src/slim/scripts/finetune_all_inception_v3_on_scoodit_178_param_test.sh
@@ -6,10 +6,7 @@
 #
 # Usage:
 # cd slim
-# ./slim/scripts/finetune_inceptionv3_on_scoodit_178.sh 32 1 4 4 1000 500
-INCEPTION_V3_LINK=http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz
-INCEPTION_V4_LINK=http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
-INCEPTION_RESNET_LINK=http://download.tensorflow.org/models/inception_resnet_v2_2016_08_30.tar.gz
+# ./slim/scripts/finetune_inceptionv3_on_scoodit_178.sh 32 1 4 4 1000
 
 # Where the pre-trained InceptionV3 checkpoint is saved to.
 PRETRAINED_CHECKPOINT_DIR=${HOME}/PycharmProjects/scoodit_image_classification/models/downloaded_tf_models
@@ -22,12 +19,10 @@ CLONES=$2
 READERS=$3
 THREADS=$4
 STEPS1=$5
-STEPS2=$6
-LR1=$7
-LR2=$8
+LR=$6
 DATASET=scoodit_178
 MODEL=inception_v3
-SUFFIX=bt_${BATCH}_cl_${CLONES}_r_${READERS}_thr_${THREADS}_lr1_${LR1}_lr2_${LR2}
+SUFFIX=bt_${BATCH}_cl_${CLONES}_r_${READERS}_thr_${THREADS}_lr_${LR}
 
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
 TRAIN_DIR=${HOME}/PycharmProjects/scoodit_image_classification/models/${MODEL}/${DATASET}/${SUFFIX}
@@ -54,7 +49,7 @@ python train_image_classifier.py \
   --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --max_number_of_steps=${STEPS1} \
-  --learning_rate=${LR1} \
+  --learning_rate=${LR} \
   --learning_rate_decay_type=fixed \
   --save_interval_secs=30 \
   --save_summaries_secs=30 \
@@ -75,32 +70,3 @@ python eval_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL}
 
-# Fine-tune all the new layers.
-python train_image_classifier.py \
-  --checkpoint_path=${TRAIN_DIR} \
-  --train_dir=${TRAIN_DIR}/all \
-  --dataset_name=${DATASET} \
-  --dataset_split_name=train \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=${MODEL} \
-  --max_number_of_steps=${STEPS2} \
-  --learning_rate=${LR2} \
-  --learning_rate_decay_type=fixed \
-  --save_interval_secs=30 \
-  --save_summaries_secs=30 \
-  --log_every_n_steps=10 \
-  --optimizer=rmsprop \
-  --weight_decay=0.00004 \
-  --batch_size=${BATCH} \
-  --num_clones=${CLONES} \
-  --num_readers=${READERS} \
-  --num_preprocessing_threads=${THREADS}
-
-# Run evaluation.
-python eval_image_classifier.py \
-  --checkpoint_path=${TRAIN_DIR}/all \
-  --eval_dir=${TRAIN_DIR}/all \
-  --dataset_name=${DATASET} \
-  --dataset_split_name=validation \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=${MODEL}
